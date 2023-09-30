d96511f954a8986f4c5ba67761a14ae2c8322533
Voldoned <vova.nederkin@gmail.com>
2023-10-01 01:57:51 +0300

    delete douplicate model

--- fastapp/app/models.py
+++ fastapp/app/models.py
@@ -1,23 +1,13 @@
 from datetime import datetime
 
-from sqlalchemy import Integer, String, TIMESTAMP, Table, Column
+from sqlalchemy import Integer, String, TIMESTAMP, Column
 
-from .database import metadata, Base
-
-user = Table(
-    "user",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("email", String, nullable=False),
-    Column("username", String, nullable=False),
-    Column("password", String, nullable=False),
-    Column("registered_at", TIMESTAMP, default=datetime.utcnow),
-)
+from .database import Base
 
 
 class User(Base):
     __tablename__ = "user"
-    __table_args__ = {'extend_existing': True}
+    # __table_args__ = {'extend_existing': True}
 
     id: int = Column(Integer, primary_key=True)
     email: str = Column(String, nullable=False)

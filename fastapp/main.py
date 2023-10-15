4be5d1160d68517e0714c4998996066fb9501b91
Voldoned <vova.nederkin@gmail.com>
2023-10-15 22:27:17 +0300

    Create lifespan for application, connect helpers from core of application. Disable CORS middleware

--- fastapp/main.py
+++ fastapp/main.py
@@ -1,30 +1,47 @@
-import uvicorn
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.router import router
-from cache import init_redis
-from app.database import init_models
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
 
-app = FastAPI()
+from redis import asyncio as aioredis
+from contextlib import asynccontextmanager
 
-origins = [
-    "http://localhost:8000"
-]
+from app.api_v1.articles.router import router as router_articles
+from app.users.router import router as router_users
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-app.include_router(router)
+from app.core.db_helper import db_helper
+from app.core.base import Base
+from app.core.db_settings import settings
 
 
-@app.on_event("startup")
-async def on_startup():
-    await init_models()
-    await init_redis()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    redis = aioredis.from_url(
+        settings.db_cache_url,
+        encoding="utf8",
+        decode_responses=True
+    )
+    FastAPICache.init(RedisBackend(redis), prefix="api:cache")
+
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+# origins = [
+#     "http://localhost:8000"
+# ]
+#
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+app.include_router(router_articles)
+app.include_router(router_users)

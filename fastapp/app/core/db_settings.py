5fb86994fb2a430d44d40553f5ae58363cd77028
voldoned <vova.nederkin@gmail.com>
2023-12-23 00:13:32 +0300

    Fix import

--- fastapp/app/core/db_settings.py
+++ fastapp/app/core/db_settings.py
@@ -1,15 +1,12 @@
 from pydantic_settings import BaseSettings
 
-from config import (
-    DB_PASSWORD, DB_USER, DB_HOST, DB_PORT, DB_NAME,
-    REDIS_HOST, REDIS_PORT
-)
+from config import settings
 
 
 class Settings(BaseSettings):
-    db_url: str = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?async_fallback=True"
-    db_cache_url: str = f"redis://{REDIS_HOST}:{REDIS_PORT}"
+    db_url: str = f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}?async_fallback=True"
+    db_cache_url: str = f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}"
     db_echo: bool = True
 
 
-settings = Settings()
+db_settings = Settings()

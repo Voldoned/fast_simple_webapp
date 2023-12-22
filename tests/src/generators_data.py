dfe4c4baf78e61799e74cd8d1fc7f9113296a17b
voldoned <vova.nederkin@gmail.com>
2023-12-23 00:24:18 +0300

    Update run-time tests locust

--- tests/src/generators_data.py
+++ tests/src/generators_data.py
@@ -1,5 +1,4 @@
 import faker
-from datetime import datetime
 
 fake = faker.Faker()
 
@@ -8,9 +7,8 @@ users_data = [
         "email": fake.unique.email(),
         "username": fake.unique.name(),
         "password": fake.unique.password(),
-        "registered_at": datetime.now().isoformat()
     }
-    for _ in range(9)
+    for _ in range(99)
 ]
 
 articles_data = [
@@ -18,9 +16,8 @@ articles_data = [
         "title": fake.unique.text(),
         "text": fake.unique.text(),
         "annotation": fake.unique.text(),
-        "published_at": datetime.now().isoformat()
     }
-    for _ in range(9)
+    for _ in range(99)
 ]
 
 bad_users_data = [
@@ -28,9 +25,8 @@ bad_users_data = [
         "email": fake.unique.random_int(),
         "username": fake.unique.name(),
         "password": fake.unique.password(),
-        "registered_at": datetime.now().isoformat()
     }
-    for _ in range(9)
+    for _ in range(90)
 ]
 
 bad_articles_data = [
@@ -38,7 +34,6 @@ bad_articles_data = [
         "title": fake.unique.random_int(),
         "text": fake.unique.text(),
         "annotation": fake.unique.text(),
-        "published_at": datetime.now().isoformat()
     }
-    for _ in range(9)
+    for _ in range(90)
 ]

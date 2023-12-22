e326b701c7283801d7c9591685b805a37e409643
voldoned <vova.nederkin@gmail.com>
2023-12-23 00:11:49 +0300

    Add update and delete operations

--- fastapp/app/core/base_crud.py
+++ fastapp/app/core/base_crud.py
@@ -1,17 +1,19 @@
 from fastapi import HTTPException
+from pydantic import BaseModel
 from sqlalchemy import select, insert
 from sqlalchemy.exc import DBAPIError
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from .base import Base
 from .db_utils import statement_select_all_from_table
 
 
 class BaseAsyncCRUD:
 
-    def __init__(self, model):
+    def __init__(self, model: Base):
         self.model = model
 
-    async def get_all_raws_from_tabel(self, session: AsyncSession):
+    async def get_all_tabel(self, session: AsyncSession):
         statement = await statement_select_all_from_table(self.model)
         result = await session.execute(statement)
         return {
@@ -43,23 +45,76 @@ class BaseAsyncCRUD:
     async def get_row_with_id(self, id: int, session: AsyncSession):
         statement = select(self.model).where(self.model.id == id)
         result = await session.execute(statement)
-        return result.scalars().all()
+        return result.scalars().all()[0]
 
-    async def create_row(self, schema, session: AsyncSession):
+    async def create_row(self, schema: BaseModel, session: AsyncSession):
         try:
-            input_data = schema.model_dump()
-            statement = insert(self.model).values(**input_data)
-            await session.execute(statement)
+            input_data = self.model(**schema.model_dump())
+            # statement = insert(self.model).values(**input_data)
+            session.add(input_data)
             await session.commit()  # End transaction
+            # await session.refresh()
             return {
                 "status": "success",
                 "new_data": input_data
             }
-        except DBAPIError as e:
+
+        except DBAPIError as ex:
             raise HTTPException(
                 status_code=400,
-                detail=f"{e}"
+                detail=f"{ex}"
             )
+
+        except Exception as ex:
+            raise HTTPException(
+                status_code=500,
+                detail=f"{ex}"
+            )
+
+    async def update_row(self,
+                         schema: BaseModel,
+                         session: AsyncSession,
+                         is_partial: bool = False):
+        try:
+            row = await self.get_row_with_id(id=schema.id, session=session)
+            for name, value in schema.model_dump(exclude_unset=is_partial).items():
+                setattr(row, name, value)
+            await session.commit()
+            # await session.refresh()
+            return {
+                "status": "success",
+                "updated_data": row
+            }
+
+        except DBAPIError as ex:
+            raise HTTPException(
+                status_code=400,
+                detail=f"{ex}"
+            )
+
+        except Exception as ex:
+            raise HTTPException(
+                status_code=500,
+                detail=f"{ex}"
+            )
+
+    async def delete_row(self, id, session: AsyncSession):
+        try:
+            row = await self.get_row_with_id(id=id, session=session)
+            await session.delete(row)
+            await session.commit()
+            # await session.refresh()
+            return {
+                "status": "success",
+                "deleted_data": row
+            }
+
+        except DBAPIError as ex:
+            raise HTTPException(
+                status_code=400,
+                detail=f"{ex}"
+            )
+
         except Exception as ex:
             raise HTTPException(
                 status_code=500,

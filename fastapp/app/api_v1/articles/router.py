f7f867911b983877eedd5d26e481ae3dfa10838e
voldoned <vova.nederkin@gmail.com>
2023-12-23 00:16:33 +0300

    Fix imports and add views for update and delete requests

--- fastapp/app/api_v1/articles/router.py
+++ fastapp/app/api_v1/articles/router.py
@@ -1,19 +1,30 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import joinedload
 
 from fastapi_cache.decorator import cache
 
-from app.core.db_helper import db_helper
-from app.core.db_utils import statement_select_all_from_table
-from app.core.base_crud import BaseAsyncCRUD
+from config import settings
+
+from .schemas.response.update_response import ArticleUpdateResponseSchema
+from ...core.db_helper import db_helper
+from ...core.db_utils import statement_select_all_from_table
+from ...core.base_crud import BaseAsyncCRUD
+
 from .models import Article
-from .schemas.response_schemas import (
-    ArticleResponseSchema,
-    ArticlesResponseSchema,
-    ArticleCountResponseSchema,
-    ArticleCreateResponseSchema
+from .dependencies import article_by_id
+
+from .schemas.response.article_response import ArticleResponseSchema
+from .schemas.response.articles_response import ArticlesResponseSchema
+from .schemas.response.count_response import ArticleCountResponseSchema
+from .schemas.response.create_response import ArticleCreateResponseSchema
+from .schemas.response.article_users_response import (
+    ArticlesWithUsersResponseSchema
 )
-from .schemas.request_schemas import ArticleCreateBaseRequestSchema
+from .schemas.response.delete_response import ArticleDeleteResponseSchema
+
+from .schemas.request.create_request import ArticleCreateRequestSchema
+from .schemas.request.update_request import ArticleUpdateRequestSchema
 
 router = APIRouter(
     prefix="/articles",
@@ -23,62 +34,101 @@ router = APIRouter(
 crud = BaseAsyncCRUD(Article)
 
 
-@router.get(
-    "/",
-    response_model=ArticlesResponseSchema
-)
-@cache(expire=60 * 5)
-async def get_articles(session: AsyncSession = Depends(
+@router.get("/", response_model=ArticlesResponseSchema)
+@cache(expire=settings.expire_cache)
+async def get_all_articles(session: AsyncSession = Depends(
     db_helper.scoped_session_dependency
 )):
-    return await crud.get_all_raws_from_tabel(session)
+    return await crud.get_all_tabel(session)
 
 
-@router.get(
-    "/head/{count}",
-    response_model=ArticlesResponseSchema
-)
-@cache(expire=60 * 5)
-async def get_head_articles(count: int,
-                            session: AsyncSession = Depends(
-                                db_helper.scoped_session_dependency
-                            )):
+@router.get("/head/{count}", response_model=ArticlesResponseSchema)
+@cache(expire=settings.expire_cache)
+async def get_head_count_articles(
+    count: int,
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency)
+):
     return await crud.get_head_rows(count, session)
 
 
-@router.get(
-    "/user/{id}",
-    response_model=ArticleResponseSchema
-)
-@cache(expire=60 * 5)
-async def get_article_with_id(id: int,
-                              session: AsyncSession = Depends(
-                                  db_helper.scoped_session_dependency
-                              )):
-    return await crud.get_row_with_id(id, session)
-
-
-@router.get(
-    "/count_articles",
-    response_model=ArticleCountResponseSchema
-)
-@cache(expire=60 * 5)
-async def get_count_of_articles(
-        session: AsyncSession = Depends(db_helper.scoped_session_dependency)
+@router.get("/article/{id}", response_model=ArticleResponseSchema)
+@cache(expire=settings.expire_cache)
+async def get_article_by_id(
+    article: Article = Depends(article_by_id)
 ):
-    statement = await statement_select_all_from_table(Article)
+    return article
+
+
+@router.get("/count_articles", response_model=ArticleCountResponseSchema)
+@cache(expire=settings.expire_cache)
+async def get_count_of_articles(
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency)
+):
+    statement = await statement_select_all_from_table(table=Article)
     result = await session.execute(statement)
     return {
         "count_articles": len(result.scalars().all())
     }
 
 
+@router.get("/all", response_model=ArticlesWithUsersResponseSchema)
+@cache(expire=settings.expire_cache)
+async def get_articles_user(
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency)
+):
+    query = (await statement_select_all_from_table(table=Article)).options(
+        joinedload(Article.user)
+    )
+    result = await session.execute(query)
+    list_rows = result.unique().scalars().all()
+    return list_rows
+
+
 @router.post(
     "/add",
     response_model=ArticleCreateResponseSchema
 )
-async def add_article(new_article: ArticleCreateBaseRequestSchema,
+async def add_article(new_article: ArticleCreateRequestSchema,
                       session: AsyncSession = Depends(
                           db_helper.scoped_session_dependency
                       )):
-    return await crud.create_row(new_article, session)
+    return await crud.create_row(schema=new_article, session=session)
+
+
+@router.put(
+    "/update",
+    response_model=ArticleUpdateResponseSchema
+)
+async def update_article(
+    article_update: ArticleUpdateRequestSchema,
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency)
+):
+    return await crud.update_row(
+        session=session,
+        schema=article_update,
+        is_partial=False
+    )
+
+
+# @router.put(
+#     "/update/partial",
+#     response_model=ArticleUpdateResponseSchema
+# )
+# async def partial_update_article(
+#     article_update: ArticleUpdateRequestSchema,
+#     session: AsyncSession = Depends(db_helper.scoped_session_dependency)
+# ):
+#     return await crud.update_row(
+#         session=session,
+#         schema=article_update,
+#         is_partial=True
+#     )
+
+
+@router.delete("/delete/{id}", response_model=ArticleDeleteResponseSchema)
+async def delete_article(
+    article_id: int,
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency)
+):
+    return await crud.delete_row(id=article_id, session=session)
+
--- fastapp/app/users/router.py
+++ fastapp/app/users/router.py
@@ -1,19 +1,39 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 
 from fastapi_cache.decorator import cache
 
-from app.core.db_helper import db_helper
-from app.core.db_utils import statement_select_all_from_table
-from app.core.base_crud import BaseAsyncCRUD
+from config import settings
+
+from .dependencies import user_by_id
+
+
+from ..core.db_helper import db_helper
+from ..core.db_utils import statement_select_all_from_table
+from ..core.base_crud import BaseAsyncCRUD
+
 from .models import User
-from .schemas.response_schemas import (
-    UserResponseSchema,
-    UsersResponseSchema,
-    UserCountResponseSchema,
-    UserCreateResponseSchema
+
+from .schemas.response.user_response import UserResponseSchema
+from .schemas.response.users_response import UsersResponseSchema
+from .schemas.response.count_response import UserCountResponseSchema
+from .schemas.response.user_article_response import (
+    UsersWithArticlesResponseSchema
 )
-from .schemas.request_schemas import UserCreateRequestSchema
+from .schemas.response.create_response import UserCreateResponseSchema
+from .schemas.response.delete_response import UserDeleteResponseSchema
+from .schemas.response.update_response import UserUpdateResponseSchema
+from .schemas.response.update_partial_response import (
+    UserPartialUpdateResponseSchema
+)
+
+from .schemas.request.create_request import UserCreateRequestSchema
+from .schemas.request.update_request import UserUpdateRequestSchema
+from .schemas.request.update_partial_request import (
+    UserPartialUpdateRequestSchema
+)
+
 
 router = APIRouter(
     prefix="/users",
@@ -24,35 +44,35 @@ crud = BaseAsyncCRUD(User)
 
 
 @router.get("/", response_model=UsersResponseSchema)
-@cache(expire=60 * 5)
-async def get_users(session: AsyncSession = Depends(
-    db_helper.scoped_session_dependency
-)):
-    return await crud.get_all_raws_from_tabel(session)
+@cache(expire=settings.expire_cache)
+async def get_users(
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency)
+):
+    return await crud.get_all_tabel(session)
 
 
 @router.get("/head/{count}", response_model=UsersResponseSchema)
-@cache(expire=60 * 5)
-async def get_first_users(count: int,
-                          session: AsyncSession = Depends(
-                              db_helper.scoped_session_dependency
-                          )):
+@cache(expire=settings.expire_cache)
+async def get_first_users(
+    count: int,
+    session: AsyncSession = Depends(
+        db_helper.scoped_session_dependency
+    )
+):
     return await crud.get_head_rows(count, session)
 
 
 @router.get("/user/{id}", response_model=UserResponseSchema)
-@cache(expire=60 * 5)
-async def get_user_with_id(id: int, session: AsyncSession = Depends(
-    db_helper.scoped_session_dependency
-)):
-    return await crud.get_row_with_id(id, session)
+@cache(expire=settings.expire_cache)
+async def get_user_by_id(user: User = Depends(user_by_id)):
+    return user
 
 
 @router.get("/count_users", response_model=UserCountResponseSchema)
-@cache(expire=60 * 5)
-async def get_count_of_users(session: AsyncSession = Depends(
-    db_helper.scoped_session_dependency
-)):
+@cache(expire=settings.expire_cache)
+async def get_count_of_users(
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency)
+):
     """
     Get count registered users.
     """
@@ -63,9 +83,62 @@ async def get_count_of_users(session: AsyncSession = Depends(
     }
 
 
+@router.get(
+    "/all",
+    # response_model=UsersWithArticlesResponseSchema
+)
+@cache(expire=settings.expire_cache)
+async def get_user_articles(
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency)
+):
+    query = (await statement_select_all_from_table(User)).options(
+        selectinload(User.articles)
+    )
+    result = await session.execute(query)
+    return result.unique().scalars().all()
+
+
 @router.post("/add", response_model=UserCreateResponseSchema)
-async def add_user(new_user: UserCreateRequestSchema,
-                   session: AsyncSession = Depends(
-                       db_helper.scoped_session_dependency
-                   )):
+async def add_user(
+    new_user: UserCreateRequestSchema,
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency)
+):
     return await crud.create_row(new_user, session)
+
+
+@router.put(
+    "/update",
+    response_model=UserUpdateResponseSchema
+)
+async def update_article(
+    article_update: UserUpdateRequestSchema,
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency)
+):
+    return await crud.update_row(
+        session=session,
+        schema=article_update,
+        is_partial=False
+    )
+
+
+# @router.put(
+#     "/update/partial",
+#     response_model=UserPartialUpdateResponseSchema
+# )
+# async def partial_update_article(
+#     article_update: UserPartialUpdateRequestSchema,
+#     session: AsyncSession = Depends(db_helper.scoped_session_dependency)
+# ):
+#     return await crud.update_row(
+#         session=session,
+#         schema=article_update,
+#         is_partial=True
+#     )
+
+
+@router.delete("/delete/{id}", response_model=UserDeleteResponseSchema)
+async def delete_article(
+    id: int,
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency)
+):
+    return await crud.delete_row(id=id, session=session)
